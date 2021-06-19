c0bad0c6f1b4660ddc5b5d2dd8886db14d077ec1
Hung Pham <tienhung278@gmail.com>
2021-06-19 16:54:37 +0700

    Create add post function

--- BlogUI/src/app/components/post/add-edit/add-edit.component.ts
+++ BlogUI/src/app/components/post/add-edit/add-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PostService } from 'src/app/services/post.service';
+import { Post } from 'src/app/entities/post';
 
 @Component({
   selector: 'app-add-edit',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-edit.component.css']
 })
 export class AddEditComponent implements OnInit {
+  post: Post = {};
 
-  constructor() { }
+  constructor(private router: Router, private services: PostService) { }
 
   ngOnInit(): void {
   }
 
+  getList(): void {
+    this.router.navigateByUrl("/");
+  }
+
+  save(titleForm: NgForm): void {
+    this.post = titleForm.value;
+    this.services.addPost(this.post).subscribe(() => {
+      this.getList();
+    });    
+  }
 }
